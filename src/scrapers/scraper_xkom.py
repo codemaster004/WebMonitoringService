45af4ecff46c8fa5bd7637f096a443c37112d2db
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-10-21 09:06:51 +0200

    Comment _get_next_page_button method in ScraperXKom, explain the overload

--- src/scrapers/scraper_xkom.py
+++ src/scrapers/scraper_xkom.py
@@ -7,5 +7,8 @@ from src.scrapers.base_scraper import BaseScraper
 class ScraperXKom(BaseScraper):
 	
 	def _get_next_page_button(self) -> WebElement:
+		""" Finds and returns the Next button on the page or None if not found """
 		found = self._driver.find_element(By.CSS_SELECTOR, self.selectors['next'])
+		# On X-kom the button can always be found on the page,
+		# however when it is the last page of pagination, it is not an <a> just a <span>
 		return found if found.tag_name == 'a' else None
